907c98d942f96ed598858acca9b5c935050bae68
Douglas <DouglasGomes123@users.noreply.github.com>
2018-12-31 00:23:28 -0200

    Update CarrierConfigLoader.java

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -458,16 +458,16 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.LIQUID_FINGERPRINT.equals(lastFingerprint)) {
+                    if (!Build.HAVOC_FINGERPRINT.equals(lastFingerprint)) {
                         log(
                                 "Build fingerprint changed. old: "
                                         + lastFingerprint
                                         + " new: "
-                                        + Build.LIQUID_FINGERPRINT);
+                                        + Build.HAVOC_FINGERPRINT);
                         clearCachedConfigForPackage(null);
                         sharedPrefs
                                 .edit()
-                                .putString(KEY_FINGERPRINT, Build.LIQUID_FINGERPRINT)
+                                .putString(KEY_FINGERPRINT, Build.HAVOC_FINGERPRINT)
                                 .apply();
                     }
                     break;
